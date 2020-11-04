e1d7e94fe885b90365aab86700aa36e99d23bbc8
oneday14 <67682904+oneday14@users.noreply.github.com>
2020-11-04 09:16:12 +0900

    ANN regressor 및 자동학습중단

--- 20201028.py
+++ 20201028.py
@@ -88,8 +88,32 @@ plot_model(model, to_file='model_ann_cancer.png',
                   show_layer_names=True)
 
 
-
 # ANN regressor - boston data 
+# 0. metric function define 및 module loading
+import tensorflow as tf
+import keras
+
+from keras.models import Sequential 
+from keras.layers.core import Dense 
+from keras.utils import np_utils 
+from sklearn.preprocessing import LabelEncoder
+
+def mse(y_true, y_pred):
+    from keras import backend
+    return backend.mean(backend.square(y_pred - y_true), axis=-1)
+
+def r_square(y_true, y_pred):
+    from keras import backend as K
+    SS_res =  K.sum(K.square(y_true - y_pred)) 
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+    return (1 - SS_res/(SS_tot + K.epsilon()))
+
+def r_square_loss(y_true, y_pred):
+    from keras import backend as K
+    SS_res =  K.sum(K.square(y_true - y_pred)) 
+    SS_tot = K.sum(K.square(y_true - K.mean(y_true))) 
+    return 1 - ( 1 - SS_res/(SS_tot + K.epsilon()))
+
 # 1. data loading
 from mglearn.datasets import load_extended_boston
 df_boston_et_x, df_boston_et_y = load_extended_boston()
@@ -115,7 +139,8 @@ model.add(Dense(1))
 
 model.compile(optimizer = 'adam', 
               loss = 'mean_squared_error',
-              metrics = ['mean_squared_error'])
+              metrics = ['mean_squared_error', mse,
+                         r_square, r_square_loss])
 
 # 5. 자동 학습 중단(EarlyStopping) 적용 
 from keras.callbacks import EarlyStopping
@@ -125,10 +150,53 @@ earlystopping = EarlyStopping(monitor="mean_squared_error",
                               verbose=1, 
                               mode='auto')
 
-model.fit(train_x, train_y, epochs = 3000, batch_size = 10,
+result = model.fit(train_x, train_y, epochs = 3000, batch_size = 10,
           validation_data=(test_x, test_y), callbacks=[earlystopping])
 
-model.fit(df_boston_et_x, df_boston_et_y, epochs = 3000, batch_size = 10,
+model.fit(train_x, train_y, epochs = 3000, batch_size = 10,
            validation_split=0.25, callbacks=[earlystopping])
 
+
+# 6. 모델 시각화
+# 1) 모델 요약
 model.summary()
+
+# 2) layer 시각화
+from keras.utils import plot_model
+plot_model(model, to_file='model_ann_boston.png', 
+                  show_shapes=True,
+                  show_layer_names=True)
+
+
+
+# 7. 모델 평가
+result.history.keys() # 모델 정보 확인
+
+plt.plot(result.history['loss'], label = 'train')
+plt.plot(result.history['val_loss'], label = 'test', c='red')
+plt.legend()
+
+plt.plot(result.history['r_square'], label = 'train')
+plt.plot(result.history['val_r_square'], label = 'test', c='red')
+plt.legend()
+
+model.evaluate(test_x, test_y)     # 전체 평가 지표 출력
+model.evaluate(test_x, test_y)[3]  # r_square: 84.53
+
+# loss, metrics에 전달되는 모든 값 출력
+
+# 실제값과 예측값의 비교
+v_y_pre = model.predict(test_x).flatten()
+
+for i in range(0,len(test_y)) :
+    print('실제값 : %s, 예측값 : %s' % (test_y[i], v_y_pre[i]))
+
+          
+# 선형 회귀와 비교
+m_reg = LinearRegression()
+m_reg.fit(train_x, train_y)
+m_reg.score(test_x, test_y)        # 60.75
+
+
+
+
